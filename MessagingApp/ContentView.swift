359c8315be2d4e3d399b1466c8dc2ad3f85086bb
ivant <ivant@capmega.com>
2021-08-19 11:20:32 -0500

    Initial project

---
+++ MessagingApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 19/08/21
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,19 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 19/08/21
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
